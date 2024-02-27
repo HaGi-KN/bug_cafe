0c678d98339d76530cbf2d12e0a1426320418b1e
HaGi-KN <kento.hagihara.1@gmail.com>
2024-02-27 22:34:24 +0900

    changing variable name

--- cafe.rb
+++ cafe.rb
@@ -25,11 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drinks_order = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+foods_order = take_order(FOODS)
 
-total = DRINKS[order1 - 1][:price] + FOODS[order2 - 1][:price]
+total = DRINKS[drinks_order - 1][:price] + FOODS[foods_order - 1][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
-
